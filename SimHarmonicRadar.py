19847035e0d021ef4d416f053e1bd5e370a44689
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-31 02:58:23 -0400

    pythonize-api

--- SimHarmonicRadar.py
+++ SimHarmonicRadar.py
@@ -3,78 +3,86 @@ import numpy as np
 import scipy.signal as signal
 from matplotlib.pyplot import figure,show,subplots
 import seaborn as sns
-sns.set_context('talk',font_scale=1.25)
+sns.set_context('talk')
 #
-from tincanradar.fwdmodel import chirp
-
+from tincanradar.fwdmodel import chirprx,chirptx
+#
 try:
     import pygame as smod
 except ImportError:
     smod = None
+#
 scfs = 48e3
 
-def harmonicradar_sim(fs,tm,fc,f0,f1,range_m,mode):
-    t = np.arange(0, tm, 1/fs)
+def harmonicradar_sim(P,range_m):
+    fs = 4*P['bm'] # [Hz] sampling frequency of RF simulation 
 
-    if 'cw' in mode:
-        harmonicradar_cw(t,fs,fc)
-    if 'fmcw' in mode:
-        harmonicradar_fmcw(t,tm,fs,f0,f1,range_m)
+    t = np.arange(0, P['tm'], 1/fs)
+
+    if 'cw' == P['mode']:
+        rx = harmonicradar_cw(t,fs,P['fc'])
+    if 'fmcw' == P['mode']:
+        rx = harmonicradar_fmcw(t,fs,P,range_m)
+        
+    return rx
 
 def harmonicradar_cw(t,fs,fc):
     ttxt = f'CW: {fc} Hz'
     #%% input
-    x = np.sin(2*np.pi*fc*t)
-    _,Pxx = signal.periodogram(x,fs)
+    tx = np.sin(2*np.pi*fc*t)
+    _,Pxx = signal.welch(tx,fs)
     #%% diode
     d = (signal.square(2*np.pi*fc*t))
     d[d<0] = 0.
     #%% output of diode
-    y = x * d
+    rx = tx * d
     #y = x; y[y<0]=0. #shorthand way to say it, same result
-    fax,Pyy = signal.periodogram(y,fs)
+    fax,Pyy = signal.periodogram(rx,fs)
 #%% results
-    plotlf(t,x,d,y,ttxt)
+    plotlf(t,tx,d,rx,ttxt)
     plots(fax,Pxx,Pyy,fc,ttxt)
+    
+    return rx
 
-def harmonicradar_fmcw(t,tm,fs,f0,f1,range_m):
-    bm = f1-f0
-    ttxt = 'FMCW: {} -> {} Hz'.format(f0,f1)
+def harmonicradar_fmcw(t,fs,P,range_m):
+    ttxt = f'FMCW: {P["f0"]} -> {P["f1"]} Hz'
     #x = chirp(t,f0,t[-1],f1,'linear') #radar transmit waveform
 #%% target harmonic radar tag
-    #t_targ = 2 * asarray(range_m)/c #round trip time delay due to target distance
-    #phase_targ = 2*pi*linspace(f0,f1,x.size)*t_targ
-    #y = chirp(t,f0,t[-1],f1,'linear',phi=degrees(phase_targ))
-    y,x = chirp(bm,tm,t,range_m, Atarg=1, nlfm=0.)
-    #y[y<0]=0. #tag transmit waveform
+    # note, receive RF frequency is at the Nth harmonic--but bx is baseband in radar receiver after downconversion
+    #tx = chirptx(P['bm'],P['tm'], t, nlfm=0.)
+    
+    bx,tx = chirprx(P['bm'],P['tm'],t,range_m,Atarg=1,nlfm=0.)
 
-    fax,Pxx = signal.periodogram(x,fs)
-    fax,Pyy = signal.periodogram(y,fs,detrend=False)
+    f,Pxx = signal.welch(tx,fs,return_onesided=False)
+    f,Pyy = signal.welch(bx,fs,return_onesided=False)
 
-    plots(fax,None,Pyy,fc,ttxt)
+    plots(f,Pxx,Pyy,None,ttxt)
 #%% radar received waveform (homodyne)
-    z = y * x.conjugate()#tag transmit waveform
+    rx = bx * tx.conjugate() # tag transmit waveform
 #%% antialias filter & resample
     b = signal.firwin(numtaps=100, cutoff=fs/4., nyq=fs/2.) # LPF
-    zf = signal.lfilter( b, 1., z)
-    zr,tz = signal.resample(zf,int(zf.size * scfs / fs),t)
-    faz,Pzz = signal.periodogram(zr,scfs,detrend=False)
+    rxf = signal.lfilter( b, 1., rx)
+    rxf,tr = signal.resample(rxf,int(rxf.size * scfs / fs),t)
+    f,Pzz = signal.welch(rxf,scfs,detrend=False,return_onesided=False)
 
-    plotif(tz,zr,faz,Pzz)
+    plotif(tr,rxf,f,Pzz)
 
-    sounds(zr)
+    return rxf
 
 
-def plotif(t,z,fax,Pzz):
+def plotif(t,x,f,Pzz,fc=None):
     fg,axs = subplots(2,1)
     ax=axs[0]
-    ax.plot(t,z)
+    ax.plot(t,x)
     ax.set_xlabel('time [sec]')
 
     ax=axs[1]
-    ax.plot(fax,Pzz)
-    ax.set_xlim(0,6*fc)
+    ax.plot(f,Pzz)
     ax.set_xlabel('frequency [Hz]')
+    
+    if fc is not None:
+        ax.set_xlim(0,6*fc)
+
 
 def plotlf(t,x,d,y,ttxt):
 
@@ -96,15 +104,18 @@ def plots(fax,Pxx,Pyy,fc,ttxt):
     fg,axs = subplots(2,1,sharex=True)
     fg.suptitle(ttxt + ' spectral power density\nhomodyne reception-zero IF')
 
-    if Pxx is not None:
-        ax=axs[0]
-        ax.plot(fax,10*np.log10(Pxx))
-        ax.set_ylabel('$P_{xx}$')
+    ax=axs[0]
+    ax.plot(fax,10*np.log10(Pxx))
+    ax.set_ylabel('$P_{tx}$')
+    ax.set_title('Transmit Spectrum')
+    ax.set_ylim(-120,None)
 
     ax=axs[1]
     ax.plot(fax,10*np.log10(Pyy))
     ax.set_xlabel('frequency [Hz]')
-    ax.set_ylabel('$P_{yy}$')
+    ax.set_ylabel('$P_{rx}$')
+    ax.set_title('Receive Spectrum')
+    ax.set_ylim(-120,None)
     #ax.set_xlim(0,6*fc)
 
  #   for ax in axs:
@@ -125,19 +136,26 @@ def sounds(x):
             print('pygame volume level:',sound.get_volume())
 
 if __name__ == '__main__':
-    mode=['fmcw']
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('range_m',help='range to target [m]',type=float,default=100.)
+    p.add_argument('-tm',help='chirp length [sec]',type=float,default=0.01)
+    p.add_argument('-m','--mode',help='radar mode (cw,fmcw)',default='fmcw')
+    p.add_argument('-fc',help='freqeuncy of baseband carrier (CW only)',type=float,default=1000.)
+    p.add_argument('-f',help='start stop frequency of chirp (FMCW only)',nargs=2,type=float,default=(902e6,928e6))
+    p = p.parse_args()
+    
+    P = {'bm':p.f[1]-p.f[0],
+         'f1':p.f[1],
+         'f0':p.f[0],
+         'fc':p.fc,
+         'tm':p.tm,
+         'mode':p.mode,
+    }
 
-    tm = 10e-3
-    fc = 1000
 
-
-    f0 = 902e6
-    f1 = 928e6
-
-    rfs=4*(f1-f0)
-
-    range_m = 50. #range to target
-
-    harmonicradar_sim(rfs,tm,fc,f0,f1,range_m,mode)
+    rx = harmonicradar_sim(P, p.range_m)
+    
+    sounds(rx)
 
     show()
