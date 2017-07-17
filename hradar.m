ca3336c2081b26319c37beddd9641ca723306483
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-07-17 16:26:15 -0400

    init

--- hradar.m
+++ hradar.m
@@ -1,10 +1,17 @@
 function hradar()
 
+range_m = 30; % meters
+
+scfs = 48e3; % Hz sampling frequency of sound card (for audio playback)
+
+
 P.tm = 0.1; % sec.
 P.f1 = 928e6; % Hz
 P.f0 = 902e6; % Hz
 P.bm = P.f1-P.f0; % Hz bandwidth of FMCW
-py.harmonicradar.
+P.mode='fmcw';
+
+py.harmonicradar.harmonicradar_sim(P,range_m,scfs)
 
 
 end % function
