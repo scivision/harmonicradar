98cdd0f315bad0906f06c8eee68a8178a2e6dce4
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-10-16 20:44:58 -0400

    travis osx

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ pip.main(['install'] + pipreq)
 from setuptools import setup #enables develop
 
 setup(name='harmonicradar',
+      packages=['harmonicradar'],
       version='0.1',
 	  description='Detect targets in very cluttered zones by listening to non-linear junction-generated harmonic',
 	  author='Michael Hirsch, Ph.D.',
 	  url='https://github.com/scivision/harmonicradar',
-      install_requires=req,
+      install_requires=req+pipreq,
       dependency_links = ['https://github.com/scivision/tincanradar/tarball/master#egg=tincanradar'],
-      packages=['harmonicradar'],
 	  )
 
